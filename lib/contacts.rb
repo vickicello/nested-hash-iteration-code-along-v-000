dd0df8375fe15c680bc7d56cd6c5437449cbc640
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 19:07:24 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,9 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+     contact_details_hash.each do |attribute, data|
+       if attribute == :favorite_ice_cream_flavors
   binding.pry
-end
-
+    end
+  end
 end
